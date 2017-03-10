6e61826c5832fbf29bcf24798aa520c7def26c75
Jacob <jacob.riggs@west-mec.org>
2017-03-10 14:55:29 -0700

    Message post retrieval was added

--- back-end/server.js
+++ back-end/server.js
@@ -6,19 +6,55 @@
 
    3/2/17 - JNR - Got the server to be able to run
    3/3/17 - JNR - Added the post functionality. Put in an endpoint for message postings.
+   Take care of the CORES errors
+
+   3/6/17 - JNR - Added MongoDB support.
+   3/8/17 - JNR - Added the Message variable to implement Mongoose
+   3/9/17 - JNR - Implemented an endpoint to retrieve messages from MongoDB
 
 */
 
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+//var mongo = require('mongodb').MongoClient;
 var port = 8080;
+
+var Message = mongoose.model('Message', {
+    msg: String
+});
 
 app.use(bodyParser.json());
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    next();
+});
+
+app.get('/api/message', getMessages);
+
 app.post('/api/message', function(req, res) {
     console.log(req.body);
+
+    //database.collection('messages').insertOne(req.body);
+    var message = new Message(req.body);
+    message.save();
+
     res.status(200);
+});
+
+function getMessages(req, res) {
+    Message.find({}).exec(function(err, result) {
+       res.send(result);
+    });
+}
+
+mongoose.connect('mongodb://localhost:27017/test', function(err, db) {
+    if (!err) {
+        console.log('Connected to MongoDB');
+    }
 });
 
 var server = app.listen(port, function() {
--- front-end/src/app/main/main.controller.js
+++ front-end/src/app/main/main.controller.js
@@ -4,15 +4,31 @@
 
    purpose: Controller for main.html page
 
+   3/3/17 - JNR - Changed the file to be able to use the $http service that is used for th postMessage() function
+   3/6/17 - JNR - Changed what the msg is set to so that it picks up what is typed in on the site.
+
+   3/9/17 - JNR - Added the getMessages to be able to draw messages from the database onto our html page.
+
 */
 
 export class MainController {
-  constructor($log) {
+  constructor($http) {
     'ngInject';
-    this.$log = $log;
+    this.$http = $http;
+    this.getMessages();
   }
 
+  getMessages() {
+    var vm = this;
+    this.$http.get('http://localhost:8080/api/message').then(function(result) {
+          vm.messages = result.data;
+        });
+}
+
   postMessage() {
-    this.$log.log('post');
+    this.$http.post('http://localhost:8080/api/message', {
+      msg: this.message
+    });
   }
+
 }
